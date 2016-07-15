2ee3abacadc0eb5afff90527bf7845fb257e2d7d
Kevin Tanner <archelon14@gmail.com>
2016-07-15 15:50:59 -0400

    Changing metric works

--- js/quote.js
+++ js/quote.js
@@ -3,9 +3,17 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=imperial&APPID=42a747e29b71f50616529415fc4e1359", function(json) {
       console.log(json);
-      //temperature in fahrenheit
+      //Display the weather
       $("#temp").html(Math.ceil(json.main.temp) + '˚' + ' <br/><br/><img width="128" src="PNG/' + json.weather[0].icon + '.png"' + ' />');
       $("#city").html(json.name + ', ' + json.sys.country + ' is experiencing ' + json.weather[0].main);
+
+      //Changing the format to celsius and fahrenheit
+      $("#cel").click(function() {
+        $("#temp").html(Math.ceil((Math.ceil(json.main.temp) - 32) / 1.8) + '˚' + ' <br/><br/><img width="128" src="PNG/' + json.weather[0].icon + '.png"' + ' />');
+      });
+      $("#fah").click(function() {
+        $("#temp").html(Math.ceil(json.main.temp) + '˚' + ' <br/><br/><img width="128" src="PNG/' + json.weather[0].icon + '.png"' + ' />');
+      });
     });
   });
 }
